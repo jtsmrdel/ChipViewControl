0716233b5fcede5b7acd1475f1e5469bcda5e0c2
JT Smrdel <jtsmrdel@gmail.com>
2018-02-24 09:20:40 -0500

    Initial Commit

---
+++ ChipViewControl/ChipViewControl.h
@@ -0,0 +1,19 @@
+//
+//  ChipViewControl.h
+//  ChipViewControl
+//
+//  Created by JT Smrdel on 2/24/18.
+//  Copyright © 2018 JT Smrdel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChipViewControl.
+FOUNDATION_EXPORT double ChipViewControlVersionNumber;
+
+//! Project version string for ChipViewControl.
+FOUNDATION_EXPORT const unsigned char ChipViewControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChipViewControl/PublicHeader.h>
+
+
